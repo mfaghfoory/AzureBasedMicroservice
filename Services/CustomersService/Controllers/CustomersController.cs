886d50b457ba12f14e8c0e67cf371564e8d3fee0
meysam.faghfouri <meysam.tolive@gmail.com>
2020-01-29 15:43:59 +0330

    add customers services

---
+++ Services/CustomersService/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using AzureBasedMicroservice.EntityFramework.Customers;
+using AzureBasedMicroservice.EntityFramework.DBContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomersService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AzureBasedMicroserviceContext _dbContext = new AzureBasedMicroserviceContext();
+
+        [HttpGet]
+        public async Task<IList<Customer>> GetAllCustomers()
+        {
+            var res = await _dbContext.Customers.ToListAsync();
+            return res;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Customer> Get(int id)
+        {
+            var res = await _dbContext.Customers.FindAsync(id);
+            return res;
+        }
+    }
+}
