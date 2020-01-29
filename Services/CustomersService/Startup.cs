b740a3eb3e6e905a1ac234fc21fc2f63ab828b27
meysam.faghfouri <meysam.tolive@gmail.com>
2020-01-29 15:44:30 +0330

    add customers services

--- Services/CustomersService/Startup.cs
+++ Services/CustomersService/Startup.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-namespace AlteringsRegistrationService
+namespace CustomersService
 {
     public class Startup
     {
@@ -27,10 +33,6 @@ namespace AlteringsRegistrationService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
             }
 
             app.UseMvc();
